aa73072c397d154872fea6386c190b84fd14904d
vivianatran <tranvivianaedu@gmail.com>
2023-11-06 19:40:16 -0800

    fill rooms but needs rooms to have their widths and heights

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,17 +1,18 @@
 package core;
 
-import tileengine.TERenderer;
 import tileengine.TETile;
 import tileengine.Tileset;
 
 import java.util.*;
 
 public class World {
-
+    private static final int maxHEIGHT = 60;
+    private static final int maxWIDTH = 100;
     private Random randomSeed;
     private int HEIGHT;
     private int WIDTH;
     private TETile[][] projWorld;
+    private int quantityofRooms;
 
 
     /**
@@ -19,46 +20,57 @@ public class World {
      *
      * @param seed
      */
+    /*initializes a world of random height and width and fills it to be  and random quantity of rooms*/
     public World (long seed) {
-        //TERenderer ter = new TERenderer();
         randomSeed = new Random(seed);
-        HEIGHT = randomSeed.nextInt(0, 1000);
-        WIDTH = randomSeed.nextInt(0, 1000);
-        //ter.initialize(WIDTH, HEIGHT);
+        HEIGHT = randomSeed.nextInt(0, maxHEIGHT);
+        WIDTH = randomSeed.nextInt(0, maxWIDTH);
         projWorld = new TETile[WIDTH][HEIGHT];
+        quantityofRooms = randomSeed.nextInt(60);
+        fillSpace(0,0, WIDTH, HEIGHT, Tileset.NOTHING);
+        }
 
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                projWorld[x][y] = Tileset.NOTHING;
+    /*fills all the possible rooms */
+    private void fillRooms() {
+        for (int i = 0; i < quantityofRooms; i++) {
+            int xLocation = randomSeed.nextInt(maxWIDTH);
+            int yLocation = randomSeed.nextInt(maxHEIGHT);
+            Room currRoom = new Room(randomSeed, this, xLocation, yLocation);
+            if (currRoom.placeable()) {
+                fillSpace(xLocation, yLocation, xLocation + currRoom.roomWIDTH, yLocation + currRoom.roomHEIGHT, Tileset.FLOOR);
             }
+
+        }
+    }
+    /*fills the world starting from the start position to wherever it will end*/
+    private void fillSpace(int startX, int startY, int endX, int endY, TETile tileType) {
+        int currentX = startX;
+        int currentY = startY;
+        while (currentY < endY) {
+            while(currentX < endX){
+                projWorld[currentX][currentY] = tileType;
+                currentX += 1;
+            }
+            currentX = startX;
+            currentY += 1;
+        }
+
+    }
+        public TETile[][] worldState () {
+            return projWorld;
+        }
+        public int worldWidth () {
+            return WIDTH;
+        }
+        public int worldHEIGHT () {
+            return HEIGHT;
+        }
+        public TETile getTile ( int x, int y){
+            return projWorld[x][y];
         }
 
 
-
-    }
-
-    public TETile[][] worldState() {
-        return projWorld;
-    }
-    public int worldWidth() {
-        return WIDTH;
-    }
-    public int worldHEIGHT() {
-        return HEIGHT;
-    }
-    public TETile getTile(int x, int y) {
-        return projWorld[x][y];
-    }
-
-
-//    public static void main(String[] args) {
-//        TERenderer ter = new TERenderer();
-//        ter.initialize(HEIGHT, WIDTH);
-//
-//        World world = new World(345);
-//
-//        ter.renderFrame(worldState);
-//    }
 }
+
 
 
