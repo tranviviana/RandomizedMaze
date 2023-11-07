28d1d973c99ac48d60b949397220119a80db00af
Edwin Kim <edwinkim0509@berkeley.edu>
2023-11-06 16:57:29 -0800

    World Class Updates

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,30 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.Tileset;
+import tileengine.TETile;
+
 public class World {
 
-    // build your own world!
+    private static final int xMin = 20;
+    private static final int xMax = 1000;
+    private static final int yMin = 20;
+    private static final int yMax = 1000;
+
+    /**
+     * Fills the world
+     * @param seed 
+     */
+    public World (long seed) {
+
+    }
+
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(xMin, xMax);
+
+        ter.renderFrame();
+    }
 
 }
