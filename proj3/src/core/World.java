a3673a0d4cf8035da1447686959a21b217899c3a
vivianatran <tranvivianaedu@gmail.com>
2023-11-08 19:14:02 -0800

    starting new approach random generation ONLY in world

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -6,52 +6,17 @@ import tileengine.Tileset;
 import java.util.*;
 
 public class World {
-    private static final int maxHEIGHT = 60;
-    private static final int maxWIDTH = 100;
-    private Random randomSeed;
-    public int HEIGHT;
+    TETile[][] projWorld;
+    Random randomGenerator;
     public int WIDTH;
-    private TETile[][] projWorld;
-    private int quantityofRooms;
-
-
-    /**
-     * Fills the world
-     *
-     * @param seed
-     */
-    /*initializes a world of random height and width and fills it to be  and random quantity of rooms*/
-    public World (long seed) {
-        randomSeed = new Random(seed);
-//        HEIGHT = 40;
-//        WIDTH = 40;
-//        quantityofRooms = 1;
-        HEIGHT = Math.abs(randomSeed.nextInt(10, maxHEIGHT));
-        WIDTH = Math.abs(randomSeed.nextInt(10, maxWIDTH));
-        projWorld = new TETile[WIDTH][HEIGHT];
-        quantityofRooms = randomSeed.nextInt(60);
-        fillSpace(0,0, WIDTH, HEIGHT, Tileset.NOTHING);
-        fillRooms();
-    }
-
-    /*fills all the possible rooms */
-    /*needs to stop creating rooms if grid is too filled NEEEED TO FINISH */
-    private void fillRooms() {
-        for (int i = 0; i < quantityofRooms; i++) {
-//            int xLocation = 10;
-//            int yLocation = 10;
-            int xLocation = randomSeed.nextInt(maxWIDTH);
-            int yLocation = randomSeed.nextInt(maxHEIGHT);
-            Room currRoom = new Room(randomSeed, this, xLocation, yLocation);
-            if (currRoom.placeable()) {
-                fillSpace(xLocation, yLocation, xLocation + currRoom.roomWIDTH(), yLocation + currRoom.roomHEIGHT(), Tileset.FLOWER);
-            }
-
-        }
-    }
+    public int HEIGHT;
     /*fills the world starting from the start position to wherever it will end*/
+    public World (Long seed) {
+        randomGenerator = new Random(seed);
+        WIDTH = randomGenerator.nextInt(10, 100);
+        HEIGHT = randomGenerator.nextInt(10, 100);
+    }
     private void fillSpace(int startX, int startY, int endX, int endY, TETile tileType) {
-
         for (int x = startX; x < endX; x++) {
             for (int y = startY; y < endY; y++) {
                 projWorld[x][y] = tileType;
