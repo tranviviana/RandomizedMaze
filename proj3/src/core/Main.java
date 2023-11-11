1050baabba706d778d7f451c2c2d238cb0716bee
vivianatran <tranvivianaedu@gmail.com>
2023-11-11 15:04:19 -0800

    main style

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -5,10 +5,14 @@ import tileengine.TERenderer;
 import tileengine.TETile;
 
 import java.awt.*;
-import java.util.*;
-import java.util.List;
+
 
 public class Main {
+    public static final int CANVASWIDTH = 1200;
+    public static final int CANVASHEIGHT = 800;
+    public static final int TITLEFONT = 60;
+    public static final int REGULARFONT = 30;
+    public static final double CENTER = 0.5;
     //doesnt process and doesnt account for pressing random letters in between
     public static void processingSeedStrokes() throws Exception {
         StringBuilder seedType = new StringBuilder();
@@ -27,17 +31,17 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        StdDraw.setCanvasSize(1200, 800);
+        StdDraw.setCanvasSize(CANVASWIDTH, CANVASHEIGHT);
         StdDraw.clear(Color.black);
         StdDraw.setPenColor(Color.white);
-        Font font = new Font("Poppins", Font.BOLD, 60);
+        Font font = new Font("Poppins", Font.BOLD, TITLEFONT);
         StdDraw.setFont(font);
-        StdDraw.text(0.5, 0.8, "CS 61B Ghost Busters");
-        font = new Font("Poppins", Font.BOLD, 35);
+        StdDraw.text(CENTER, CENTER + 0.3, "CS 61B Ghost Busters");
+        font = new Font("Poppins", Font.BOLD, REGULARFONT);
         StdDraw.setFont(font);
-        StdDraw.text(0.5, 0.3, "New Game (N)");
-        StdDraw.text(0.5, 0.2, "Load Game (L)");
-        StdDraw.text(0.5, 0.1, "Quit (Q)");
+        StdDraw.text(CENTER, CENTER - 0.2, "New Game (N)");
+        StdDraw.text(CENTER, CENTER - 0.3, "Load Game (L)");
+        StdDraw.text(CENTER, CENTER - 0.4, "Quit (Q)");
         while (StdDraw.hasNextKeyTyped()) {
             char c = StdDraw.nextKeyTyped();
             if (c == 'N' || c == 'n') {
