baf6885b1eec7adee41f7d9fd968ef62bf51752f
vivianatran <tranvivianaedu@gmail.com>
2023-11-10 18:10:21 -0800

    style check

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -114,7 +114,6 @@ public class World {
                                   projWorld[x][y] = WALLREP;
                         }
                     }
-
                 }
             }
         }
