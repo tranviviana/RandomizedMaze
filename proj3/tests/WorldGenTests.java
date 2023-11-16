e794f0a0c57a27df89dd8b244fbf309597d4a739
vivianatran <tranvivianaedu@gmail.com>
2023-11-16 15:58:33 -0800

    here edwin

--- proj3/tests/WorldGenTests.java
+++ proj3/tests/WorldGenTests.java
@@ -33,9 +33,19 @@ public class WorldGenTests {
     @Test
     public void basicSaveTest() {
         // TODO: write a test that calls getWorldFromInput twice, with "n123swasd:q" and with "lwasd"
-        TETile[][] tiles = AutograderBuddy.getWorldFromInput("n1111sw:qlw:qlw");
-        tiles = AutograderBuddy.getWorldFromInput("lwasw:ql:qlddd");
-       tiles = AutograderBuddy.getWorldFromInput("lwasds");
+        //TETile[][] tiles = AutograderBuddy.getWorldFromInput("N999SDDDWWWDDD");
+//
+//        TETile[][] tiles  = AutograderBuddy.getWorldFromInput("N999SDDD:Q");
+//        tiles = AutograderBuddy.getWorldFromInput("LWWWDDD");
+
+//        TETile[][] tiles  = AutograderBuddy.getWorldFromInput("N999SDDD:Q");
+//        tiles = AutograderBuddy.getWorldFromInput("LWWW:Q");
+//        tiles = AutograderBuddy.getWorldFromInput("LDDD:Q");
+//
+        TETile[][] tiles  = AutograderBuddy.getWorldFromInput("N999SDDD:Q");
+        tiles = AutograderBuddy.getWorldFromInput("L:Q");
+        tiles = AutograderBuddy.getWorldFromInput("LWWWDDD");
+
         TERenderer ter = new TERenderer();
         ter.initialize(tiles.length, tiles[0].length);
         ter.renderFrame(tiles);
