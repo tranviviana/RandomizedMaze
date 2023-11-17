2f79390d4748a35283f4910e0e269a61a52a87d3
vivianatran <tranvivianaedu@gmail.com>
2023-11-16 16:10:20 -0800

    fail safe if loading without anything

--- proj3/src/core/AutoGraderReader.java
+++ proj3/src/core/AutoGraderReader.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AutoGraderReader {
-    private List<Character> actions;
+    public List<Character> actions;
 
     public AutoGraderReader(String input) {
         actions = new ArrayList<>();
