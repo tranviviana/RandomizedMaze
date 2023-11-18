3fbfb98e850f8fa9f48979ef4c15de5513d86a75
vivianatran <tranvivianaedu@gmail.com>
2023-11-18 12:02:38 -0800

    reverting to old commit

--- proj3/src/core/AutoGraderReader.java
+++ proj3/src/core/AutoGraderReader.java
@@ -1,10 +1,16 @@
 package core;
 
+import edu.princeton.cs.algs4.StdDraw;
+import tileengine.TERenderer;
+import tileengine.TETile;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class AutoGraderReader {
-    private List<Character> actions;
+    public static List<Character> actions;
+
+    public static boolean isReplay;
 
     public AutoGraderReader(String input) {
         actions = new ArrayList<>();
@@ -21,6 +27,7 @@ public class AutoGraderReader {
                 //short circuiting for autograder
                 AutograderBuddy.getWorldFromInput(":q");
             }
+            // actions.add('b');
             return loadNewWorld(oldWorld);
         } else {
             World newWorld = new World(getSeed());
@@ -28,7 +35,7 @@ public class AutoGraderReader {
         }
     }
     /*returns the long needed to generate the new world*/
-    private long getSeed() {
+    public static long getSeed() {
         StringBuilder seed = new StringBuilder();
         if (actions.get(0) == 'n' || actions.get(0) == 'N') {
             actions.remove(0);
@@ -41,8 +48,8 @@ public class AutoGraderReader {
         return Long.parseLong(seed.toString());
     }
     /*generates the newWorld status based off of old world. For example if in my old file I moved left twice,
-    *I move left
-    *twice in this new world*/
+     *I move left
+     *twice in this new world*/
 
     private World loadNewWorld(World oldWorld) {
         while (!actions.isEmpty()) {
@@ -66,5 +73,17 @@ public class AutoGraderReader {
         }
         return oldWorld;
     }
-}
 
+    public void loadReplayWorld() {
+        if (actions.get(0) == 'r' || actions.get(0) == 'R') {
+            World oldWorld = Main.reload();
+            actions.remove(0);
+        } else {
+            if (isReplay) {
+                World.worldReplay();
+                isReplay = false;
+            }
+        }
+
+    }
+}
--- proj3/tests/WorldGenTests.java
+++ proj3/tests/WorldGenTests.java
@@ -1,39 +1,55 @@
+
 import core.AutograderBuddy;
 import core.World;
+
 import edu.princeton.cs.algs4.StdDraw;
 import org.junit.jupiter.api.Test;
+import spark.utils.IOUtils;
 import tileengine.TERenderer;
 import tileengine.TETile;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 
 public class WorldGenTests {
 
     public boolean isGameOver = true;
     @Test
-    public void basicTest() {
-        TETile[][] tiles = AutograderBuddy.getWorldFromInput("n123412s");
-        TERenderer ter = new TERenderer();
-        ter.initialize(tiles.length, tiles[0].length);
-        ter.renderFrame(tiles);
-        StdDraw.pause(1000);
+    public void basicTest() throws Exception {
         // put different seeds here to test different worlds
-        // pause for 5 seconds so you can see the output
-    }
-
-    @Test
-    public void basicInteractivityTest() {
-        // TODO: write a test that uses an input like "n123swasdwasd"
-        TETile[][] tiles = AutograderBuddy.getWorldFromInput("n123412swwwwwwwww");
+        TETile[][] tiles = AutograderBuddy.getWorldFromInput("n1111");
         TERenderer ter = new TERenderer();
         ter.initialize(tiles.length, tiles[0].length);
         ter.renderFrame(tiles);
-        StdDraw.pause(800);
 
-    }
 
-    @Test
-    public void basicSaveTest() {
-        // TODO: write a test that calls getWorldFromInput twice, with "n123swasd:q" and with "lwasd"
-        //TETile[][] tiles = AutograderBuddy.getWorldFromInput("N999SDDDWWWDDD");
+        public void basicTest () {
+            TETile[][] tiles = AutograderBuddy.getWorldFromInput("n123412s");
+            TERenderer ter = new TERenderer();
+            ter.initialize(tiles.length, tiles[0].length);
+            ter.renderFrame(tiles);
+            StdDraw.pause(1000);
+            // put different seeds here to test different worlds
+            // pause for 5 seconds so you can see the output
+
+        }
+
+        @Test
+        public void basicInteractivityTest () {
+            // TODO: write a test that uses an input like "n123swasdwasd"
+            TETile[][] tiles = AutograderBuddy.getWorldFromInput("n123412swwwwwwwww");
+            TERenderer ter = new TERenderer();
+            ter.initialize(tiles.length, tiles[0].length);
+            ter.renderFrame(tiles);
+            StdDraw.pause(800);
+
+        }
+
+        @Test
+        public void basicSaveTest () {
+            // TODO: write a test that calls getWorldFromInput twice, with "n123swasd:q" and with "lwasd"
+            //TETile[][] tiles = AutograderBuddy.getWorldFromInput("N999SDDDWWWDDD");
 //
 //        TETile[][] tiles  = AutograderBuddy.getWorldFromInput("N999SDDD:Q");
 //        tiles = AutograderBuddy.getWorldFromInput("LWWWDDD");
@@ -42,13 +58,14 @@ public class WorldGenTests {
 //        tiles = AutograderBuddy.getWorldFromInput("LWWW:Q");
 //        tiles = AutograderBuddy.getWorldFromInput("LDDD:Q");
 //
-        TETile[][] tiles  = AutograderBuddy.getWorldFromInput("N999SDDD:Q");
-        tiles = AutograderBuddy.getWorldFromInput("L:Q");
-        tiles = AutograderBuddy.getWorldFromInput("LWWWDDD");
+            TETile[][] tiles = AutograderBuddy.getWorldFromInput("N999SDDD:Q");
+            tiles = AutograderBuddy.getWorldFromInput("L:Q");
+            tiles = AutograderBuddy.getWorldFromInput("LWWWDDD");
 
-        TERenderer ter = new TERenderer();
-        ter.initialize(tiles.length, tiles[0].length);
-        ter.renderFrame(tiles);
-        StdDraw.pause(10000000);
+            TERenderer ter = new TERenderer();
+            ter.initialize(tiles.length, tiles[0].length);
+            ter.renderFrame(tiles);
+            StdDraw.pause(10000000);
+        }
     }
 }
